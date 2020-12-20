4cccb027f4ea6590a748297ecf1bcbf63323a7ce
XiongHaiyang <18756618469@163.com>
2020-12-20 11:31:13 +0800

    first commit

---
+++ project_parent/spark/src/main/scala/com/sean/structuredstreaming/WordCount1.scala
@@ -0,0 +1,31 @@
+package com.sean.structuredstreaming
+
+import org.apache.spark.sql.streaming.StreamingQuery
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object WordCount1 {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[2]")
+      .appName("WordCount1")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    import spark.implicits._
+    val lines: DataFrame = spark.readStream
+      .format("socket")
+      .option("host", "hadoop121")
+      .option("port", 9999)
+      .load
+    val wordCounts: DataFrame = lines.as[String]
+      .flatMap(_.split(" "))
+      .groupBy("value")
+      .count()
+    val query: StreamingQuery = wordCounts.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+    query.awaitTermination()
+    spark.stop()
+  }
+}
