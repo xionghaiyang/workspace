6e16d3bc12ba02a126c9482afbb7899e65b7f7df
XiongHaiyang <18756618469@163.com>
2020-12-20 12:12:07 +0800

    ReadFromFile2

---
+++ project_parent/spark/src/main/scala/com/sean/structuredstreaming/ReadFromFile2.scala
@@ -0,0 +1,30 @@
+package com.sean.structuredstreaming
+
+import org.apache.spark.sql.streaming.{StreamingQuery, Trigger}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+object ReadFromFile2 {
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[2]")
+      .appName("ReadFromFile")
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val userSchema: StructType = new StructType().add("name", StringType)
+      .add("age", IntegerType)
+      .add("adress", StringType)
+
+    val user: DataFrame = spark.readStream
+      .schema(userSchema)
+      .csv("D://bigdata//data")
+
+    val query: StreamingQuery = user.writeStream
+      .outputMode("append")
+      .trigger(Trigger.ProcessingTime(0))
+      .format("console")
+      .start()
+    query.awaitTermination()
+  }
+}
