572d6b2120eb88b8e72e6f0201c24c47406cb8dc
XiongHaiyang <18756618469@163.com>
2020-12-26 22:37:15 +0800

    ClassDemo03

---
+++ project_parent/scala/src/main/scala/com/sean/chapter9/ClassDemo03.scala
@@ -0,0 +1,21 @@
+package com.sean.chapter9
+
+import scala.collection.mutable.ArrayBuffer
+
+object ClassDemo03 {
+  def main(args: Array[String]): Unit = {
+    val arr: ArrayBuffer[String] = ArrayBuffer("hadoop", "spark", "flink")
+    println(s"arr:${arr}")
+    arr += "flume"
+    arr += "hadoop"
+    println(s"arr:${arr}")
+    arr -= "hadoop"
+    arr -= "hadoop"
+    println(s"arr:${arr}")
+    arr ++= Array("hive", "sqoop")
+    println(s"arr:${arr}")
+    arr --= Array("sqoop", "spark")
+    println(s"arr:${arr}")
+  }
+
+}
