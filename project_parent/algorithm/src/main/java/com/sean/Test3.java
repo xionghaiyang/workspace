66055d9374143936cc15f19c17d2306ae431265e
XiongHaiyang <18756618469@163.com>
2020-12-26 19:33:03 +0800

    Test3

---
+++ project_parent/algorithm/src/main/java/com/sean/Test3.java
@@ -0,0 +1,20 @@
+package com.sean;
+
+public class Test3 {
+
+    ListNode left;
+
+    boolean isPalindrome(ListNode head) {
+        left = head;
+        return traverse(head);
+    }
+
+    boolean traverse(ListNode right) {
+        if (right == null) return true;
+        boolean res = traverse(right.next);
+        res = res && (right.val == left.val);
+        left = left.next;
+        return res;
+    }
+
+}
