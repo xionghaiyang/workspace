1d3ec2b9f9ce41f6b4be50b69cce5a73e8523557
XiongHaiyang <18756618469@163.com>
2020-12-26 23:31:12 +0800

    ClassDemo13

---
+++ project_parent/scala/src/main/scala/com/sean/chapter9/ClassDemo13.scala
@@ -0,0 +1,12 @@
+package com.sean.chapter9
+
+object ClassDemo13 {
+  def main(args: Array[String]): Unit = {
+    val names: List[String] = List("张三", "李四", "王五")
+    val ages: List[Int] = List(23, 24, 25)
+    val list1: List[(String, Int)] = names.zip(ages)
+    val tuple1: (List[String], List[Int]) = list1.unzip
+    println(s"拉链:$list1")
+    println(s"拉开:$tuple1")
+  }
+}
