bffd1a8eda4e5c7c068529164718db57c6055c2c
XiongHaiyang <18756618469@163.com>
2020-12-26 23:24:56 +0800

    ClassDemo11

---
+++ project_parent/scala/src/main/scala/com/sean/chapter9/ClassDemo11.scala
@@ -0,0 +1,17 @@
+package com.sean.chapter9
+
+object ClassDemo11 {
+  def main(args: Array[String]): Unit = {
+    val list1: List[Int] = List(1, 2, 3, 4)
+    println(s"isEmpty:${list1.isEmpty}")
+    val list2: List[Int] = List(4, 5, 6)
+    val list3: List[Int] = list1 ++ list2
+    println(s"list3:${list3}")
+    println(s"head:${list3.head}")
+    println(s"tail:${list3.tail}")
+    val list4: List[Int] = list3.reverse
+    println(s"list4:${list4}")
+    println(s"take:${list3.take(3)}")
+    println(s"drop:${list3.drop(3)}")
+  }
+}
