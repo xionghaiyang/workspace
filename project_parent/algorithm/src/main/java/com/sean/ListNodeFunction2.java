9b1a01c027f349a0c04b69520d040db0fb148673
XiongHaiyang <18756618469@163.com>
2020-12-26 17:30:48 +0800

    ListNodeFunction2

---
+++ project_parent/algorithm/src/main/java/com/sean/ListNodeFunction2.java
@@ -0,0 +1,59 @@
+package com.sean;
+
+public class ListNodeFunction2 {
+
+    /**
+     * 反转以a为头节点的链表
+     *
+     * @param a
+     * @return
+     */
+    ListNode reverse(ListNode a) {
+        ListNode pre, cur, nxt;
+        pre = null;
+        cur = a;
+        nxt = a;
+        while (cur != null) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+    /**
+     * 反转区间[a,b)的元素。注意是左闭右开
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre, cur, nxt;
+        pre = null;
+        cur = a;
+        nxt = a;
+        while (cur != b) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+    ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode a, b;
+        a = b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) return head;
+            b = b.next;
+        }
+        ListNode newHead = reverse(a, b);
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
+
+}
