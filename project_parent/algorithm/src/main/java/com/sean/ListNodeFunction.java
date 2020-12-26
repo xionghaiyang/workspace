5891bf66ff98f297e484a4d0cfe8759512f47253
XiongHaiyang <18756618469@163.com>
2020-12-26 17:07:07 +0800

    ListNodeFunction

---
+++ project_parent/algorithm/src/main/java/com/sean/ListNodeFunction.java
@@ -0,0 +1,39 @@
+package com.sean;
+
+public class ListNodeFunction {
+    /**
+     * 递归反转整个列表
+     *
+     * @param head
+     * @return
+     */
+    ListNode reverse(ListNode head) {
+        if (head.next == null) return head;
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+    ListNode successor = null;
+
+    ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+    ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+
+}
